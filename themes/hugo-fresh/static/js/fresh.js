d49870c1b60338afdad2b92f1bd1fdbe5b014c8d
Rafael Kerr <rafael.kerr@gmail.com>
2019-12-13 17:54:21 -0300

    MAR-1234 - testes

--- themes/hugo-fresh/static/js/fresh.js
+++ themes/hugo-fresh/static/js/fresh.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    
+
     //Preloader
     $(window).on('load', function() { // makes sure the whole site is loaded 
         $('#status').fadeOut(); // will first fade out the loading animation 
